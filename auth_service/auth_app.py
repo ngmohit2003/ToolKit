f8a0844c814a4104463729a817ed887289fbcdb1
Your Name <mohitnigote461001@gmail.com>
2026-04-18 17:48:51 +0530

    cors added successfully

--- auth_service/auth_app.py
+++ auth_service/auth_app.py
@@ -15,7 +15,10 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://127.0.0.1:5173"
+        "http://127.0.0.1:5173",
+        "https://toolkit-5bjf.onrender.com"
+
+
     ],
     allow_credentials=True,
     allow_methods=["*"],
